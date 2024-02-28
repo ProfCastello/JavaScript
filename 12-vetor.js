621bb1a6ee8e71a732f46dd7d74849d94d0ce4a3
ProfCastello <gustavo.castello@sp.senai.br>
2024-02-28 07:50:43 -0300

    alterações 27-02-2024

--- 12-vetor.js
+++ 12-vetor.js
@@ -1,6 +1,7 @@
 //Criando variáveis do tipo array (vetores)
 
 var dinos = ["Tiranossauro Rex", "Estegossauro", "Anquilossauro"];
+console.log(dinos.length);
 
 console.log(dinos); //retorna todos os itens do array
 console.log(dinos[0]); //retorna o primeiro indice do array
@@ -33,3 +34,39 @@ console.table(dinos);
 
 dinos.splice(1, 1); //remove o elemento a partir do índice 1, contando 1.
 console.table(dinos);
+
+console.log("");
+console.log("---------Continuação Aula---------");
+var dinos = [
+  "Tricerátops",
+  "Tiranossauro Rex",
+  "Estegossauro",
+  "Anquilossauro",
+  "Brontossauro",
+  "Ictiossauro",
+  "Pterodáctilo",
+  "Espinossauro",
+];
+
+console.table(dinos);
+
+var elementoProcurado = "Brontossauro";
+var posicao = dinos.indexOf(elementoProcurado); //retorna o valor do índice da tabela
+console.log("O " + elementoProcurado + " está no índice " + posicao);
+
+var elementoProcurado = "Pterodáctilo";
+var posicao = dinos.indexOf(elementoProcurado);
+console.log("O " + elementoProcurado + " está no índice " + posicao);
+
+var elementoProcurado = "Tricerátops";
+var posicao = dinos.indexOf(elementoProcurado);
+console.log("O " + elementoProcurado + " está no índice " + posicao);
+
+//criando cópia de array (vetor)
+var copia1 = dinos.slice(); //.slice() faz uma copia real do item de vetor
+console.log("Vetor cópia 1");
+console.table(copia1);
+
+var copia2 = dinos.slice(1, 3); //.slice(1,3) determino onde começa e onde termina a copia
+console.log("Vetor copia 2");
+console.table(copia2);
---
+++ atividade.js
@@ -0,0 +1,77 @@
+//01-Criar array
+var frutas = ["Banana", "Maçã", "Pera", "Uva", "Morango"];
+console.table(frutas);
+
+//02-adicionar Tangerina
+frutas.push("Tangerina");
+console.table(frutas);
+
+//03-Adicionar Goiaba no inicio]
+frutas.unshift("Goiaba");
+console.table(frutas);
+
+//04-Mostrar indice 5
+console.log(frutas[5]);
+
+//05-Excluir Uva
+// frutas.splice(4, 1);
+
+var position = frutas.indexOf("Uva");
+console.log(position);
+frutas.splice(position, 1);
+console.table(frutas);
+
+//Cópia de array
+var copia = frutas.slice(2, 5);
+console.table(copia);
+
+//crie e imprima 5 elementos numericos
+
+var number = [5, 4, 3, 2, 1];
+console.table(number);
+
+//imprimir o 3 elemento
+console.log(number[2]);
+
+//copia do vetor
+var copiaNumber = number.slice();
+console.table(copiaNumber);
+
+//dobro do original
+copiaNumber[0] = number[0] * 2;
+copiaNumber[1] = number[1] * 2;
+copiaNumber[2] = number[2] * 2;
+copiaNumber[3] = number[3] * 2;
+copiaNumber[4] = number[4] * 2;
+console.table(copiaNumber);
+
+console.log("------------------------");
+//crie uma matriz 3x3
+var matriz = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+console.table(matriz);
+
+//Exibir ao usuário valores das diagonais (4)
+console.log(matriz[0][0], matriz[1][1], matriz[2][2]);
+console.log(matriz[0][2], matriz[1][1], matriz[2][0]);
+console.log(matriz[2][0], matriz[1][1], matriz[0][2]);
+console.log(matriz[2][2], matriz[1][1], matriz[0][0]);
+
+// Crie um sistema de cadastro de usuário contendo uma única variável, 
+// onde os itens abaixo fiquem na respectiva ordem:
+// Nome
+// Idade
+// Cor Favorita
+// Fruta Favorita
+// Endereço
+// Nome do Pet
+
+// Exiba ao usuário os seguintes itens:
+
+// Olá "Fulano"! Seja bem-vindo ao nosso sistema.
+// Você mora no endereço "Endereço", e tem um pet chamado "pet".
+// Sua idade é "idade" e gosta muito da fruta "fruta".
+// Sua cor favorita é "Cor Favorita".
